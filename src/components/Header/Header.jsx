25cd4458626e6fcb8878f5d3b7d493d07e3b5651
Stanislav Mankovskyi <stanislav.mankovskyi@bbconsult.co.uk>
2023-05-11 15:33:50 +0100

    change js

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,58 @@
 import { NavLink, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import '../../styles/style.css';
 import moon from '../../images/moon.svg';
 import white from '../../images/white.svg';
 
+const LOCALSTORAGE_KEY = 'ui-theme';
+
+const themeBtn = document.querySelector('.theme-btn');
+const hamburgerMenu = document.querySelector('.hamburger');
+const mobileNavMenu = document.querySelector('.nav__list');
+const bodyDarkTheme = document.querySelector('body');
+
+function onClickHamburgerMenu() {
+  hamburgerMenu.classList.toggle('hamburger--active');
+  mobileNavMenu.classList.toggle('nav__list--active');
+}
+
 export const Header = () => {
+  const [themeChange, setThemeChange] = useState('');
+
+  const themeBtn = document.querySelector('.theme-btn');
+  const hamburgerMenu = document.querySelector('.hamburger');
+  const mobileNavMenu = document.querySelector('.nav__list');
+  const bodyDarkTheme = document.querySelector('body');
+
+  function chekLocalStor() {
+    const theme = window.localStorage.getItem(LOCALSTORAGE_KEY);
+    console.log(theme);
+    if (theme === 'dark') {
+      setThemeChange('dark');
+
+      bodyDarkTheme.classList.add('dark');
+    } else {
+      setThemeChange('light');
+
+      bodyDarkTheme.classList.remove('dark');
+    }
+  }
+
+  function onThemeToggle() {
+    themeBtn.classList.toggle('theme-btn--active');
+
+    const isDark = bodyDarkTheme.classList.toggle('dark');
+    if (isDark) {
+      window.localStorage.setItem(LOCALSTORAGE_KEY, 'dark');
+    } else {
+      window.localStorage.setItem(LOCALSTORAGE_KEY, 'light');
+    }
+  }
+
+  useEffect(() => {
+    chekLocalStor();
+  }, []);
+
   return (
     <header className="header">
       <nav className="nav">
@@ -35,12 +84,12 @@ export const Header = () => {
                 </NavLink>
               </li>
             </ul>
-            <div className="hamburger">
+            <div className="hamburger" onClick={onClickHamburgerMenu}>
               <span className="bar"></span>
               <span className="bar"></span>
               <span className="bar"></span>
             </div>
-            <button className="theme-btn" type="button">
+            <button className="theme-btn" type="button" onClick={onThemeToggle}>
               <img src={white} alt="" className="theme-btn__icon" />
               <img src={moon} alt="" className="theme-btn__icon" />
             </button>
